3244883358d5248028bf65965ad0999f849701c5
picafern-commits <pica.fern@gmail.com>
2026-04-15 11:18:26 +0100

    Delete js directory

--- js/auth-config.js
+++
@@ -1,5 +0,0 @@
-export const authUsers = [
-  { username: "Ricardo", password: "2297", role: "master_admin" },
-  { username: "admin", password: "admin123", role: "admin" },
-  { username: "user", password: "user123", role: "user" }
-];
