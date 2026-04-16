2d1a3a0b31d29236855c156e35d149cf932b44ec
picafern-commits <pica.fern@gmail.com>
2026-04-16 18:30:24 +0100

    Add files via upload

---
+++ Gestao/js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbiMRCFhD9WC4Z17fc9M9uXdY8ETUO05Q",
+  authDomain: "app-pai-feb73.firebaseapp.com",
+  projectId: "app-pai-feb73",
+  storageBucket: "app-pai-feb73.firebasestorage.app",
+  messagingSenderId: "594270955224",
+  appId: "1:594270955224:web:85d1d530889704d0f359ad"
+};
